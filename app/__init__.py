6809682a11b0a517fe95b0464e6e814360802110
Hector <hfelipec@miumg.edu.gt>
2021-04-25 22:04:06 -0600

    add swagger

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import reqparse, abort, Api, Resource
 
 from app.controllers.c_persona import Persona
@@ -9,4 +9,11 @@ api = Api(app)
 ##
 ## Actually setup the Api resource routing here
 ##
+
+
+@app.route('/')
+def get_docs():
+    return render_template('swaggerui.html')
+
+
 api.add_resource(Persona, '/personas')
